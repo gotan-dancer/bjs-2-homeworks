763653e73dd1e151c4ef305784e7c323b1a10ff5
gotan-dancer <eugene.kalugin@gmail.com>
2024-06-03 16:19:30 +0500

    Task 1.1 completed

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,6 +2,17 @@
 function solveEquation(a, b, c) {
   let arr = [];
   
+  let d = b * b - 4 * a *c;
+
+  if (d === 0) {
+    arr = [ (-1) * b / ( 2 * a) ];
+  }
+  else{
+    if (d > 0){
+      arr = [ (-b + Math.sqrt(d)) / ( 2 * a), (-b - Math.sqrt(d)) / ( 2 * a) ];
+    }
+  }
+
   return arr;
 }
 
