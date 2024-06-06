3f88172f93c904ddf059b3a34ad6f61b6ce8f022
gotan-dancer <eugene.kalugin@gmail.com>
2024-06-06 12:48:42 +0500

    Changed else if, Number

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -7,10 +7,8 @@ function solveEquation(a, b, c) {
   if (d === 0) {
     arr = [ (-1) * b / ( 2 * a) ];
   }
-  else{
-    if (d > 0){
+  else if (d > 0){
       arr = [ (-b + Math.sqrt(d)) / ( 2 * a), (-b - Math.sqrt(d)) / ( 2 * a) ];
-    }
   }
 
   return arr;
@@ -18,11 +16,11 @@ function solveEquation(a, b, c) {
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
-  let percentNorm = (percent / (100 * 12)).toFixed(2);
+  let percentNorm = percent / (100 * 12);
 
   let retFull = amount - contribution;
 
   let retMonth = retFull * (percentNorm + (percentNorm / (((1 + percentNorm)**countMonths) - 1)));
 
-  return (retMonth * countMonths).toFixed(2);
+  return Number((retMonth * countMonths).toFixed(2));
 }
