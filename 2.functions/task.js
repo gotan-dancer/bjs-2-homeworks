4d3aaf0372cf900d38484bd23d6f854e2caa3a2d
gotan-dancer <eugene.kalugin@gmail.com>
2024-06-11 15:52:18 +0500

    Task 2.1 completed

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,5 +1,23 @@
 function getArrayParams(...arr) {
 
+  let min, max, avg, sum, count;
+
+  min = max = avg = sum = arr[0];
+
+  for(let i = 1; i < arr.length; i++) {
+    if (arr[i] > max){
+      max = arr[i];
+    }
+
+    if(arr[i] < min){
+      min = arr[i];
+    }
+
+    sum += arr[i];
+  }
+
+  avg = Number((sum / arr.length).toFixed(2));
+
   return { min: min, max: max, avg: avg };
 }
 
