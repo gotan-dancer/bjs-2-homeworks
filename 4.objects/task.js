79e4cbec59b474b089a9fbd0ad30f9437917e5e6
gotan-dancer <eugene.kalugin@gmail.com>
2024-07-27 15:34:28 +0500

    Task 4 completed

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,34 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if(Student.hasOwnProperty(this.marks)){
+    this.marks = [...marks];
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+  if(Student.hasOwnProperty(this.marks) || (this.marks == [])){
+    this.getAverage = 0;
+  }else{
+    let sum = 0;
+    for(let i = 0; i < this.marks.length; i++){
+      sum += this.marks[i];
+    }
+    this.getAverage = sum / this.marks.length; 
+  }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
