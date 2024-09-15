0186b76154bd656ec63e1c2f7cb7af539735ed52
gotan-dancer <eugene.kalugin@gmail.com>
2024-09-15 21:05:39 +0500

    Function parseCount writed

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,9 @@
-﻿
+﻿function parseCount(value){
+    let parseValue = Number.parseFloat(value);
+
+    if(parseValue === "NaN"){
+        throw new Error("Невалидное значение");
+    }
+
+    return parseValue;
+}
